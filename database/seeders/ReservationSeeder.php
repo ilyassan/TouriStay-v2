60daf984dd2f33e8715db69d934b97a7720177ee
ilyass anida <anidailyass@gmail.com>
2025-03-03 21:26:16 +0000

    feat: (TOUR-41) Get data dynamically in the Proprietor reservations page.

--- database/seeders/ReservationSeeder.php
+++ database/seeders/ReservationSeeder.php
@@ -6,7 +6,6 @@ use App\Models\Property;
 use App\Models\Reservation;
 use App\Models\User;
 use Carbon\Carbon;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Arr;
 
