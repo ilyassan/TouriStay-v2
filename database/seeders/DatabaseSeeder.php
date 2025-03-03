c81061316e5e65ce2f9e6668e8b0b85e963615a3
ilyass anida <anidailyass@gmail.com>
2025-03-03 20:36:03 +0000

    feat: (TOUR-40) Create the factory and seeders for the database migrations.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,6 +22,7 @@ class DatabaseSeeder extends Seeder
             CitySeeder::class,
             PropertySeeder::class,
             FavoriteSeeder::class,
+            ReservationSeeder::class,
         ]);
     }
 }
---
+++ database/seeders/ReservationSeeder.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Property;
+use App\Models\Reservation;
+use App\Models\User;
+use Carbon\Carbon;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Arr;
+
+class ReservationSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $properties = Property::all();
+        $users = User::pluck("id")->toArray();
+
+        $properties->each(function($property) use ($users){
+            $from = Carbon::parse($property->available_from);
+            $to = Carbon::parse($property->available_to);
+            $availabilityDays = $from->diffInDays($to);
+
+            $addedDays = rand(1, ceil($availabilityDays / 2));
+            $fromDate = $from->addDays($addedDays);
+            $toDate = $fromDate->copy()->addDays(rand(0, $availabilityDays - $addedDays));
+
+            $reservation = Reservation::create([
+                "user_id" => Arr::random($users),
+                "property_id" => $property->id,
+                "from_date" => $fromDate,
+                "to_date" => $toDate,
+                "total_price" => $property->price * $fromDate->diffInDays($toDate),
+            ]);
+        });
+    }
+}
